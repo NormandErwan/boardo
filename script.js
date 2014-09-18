0c9fe3da1b6f9c88a44382e109c332492df30516
Erwan Normand <normand.erwan@gmail.com>
2014-09-18 22:24:31 +0200

    Some comments & fixes

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-function addEvent(obj, event, func, arg) {
-    if (obj.attachEvent) //IE
-        obj.attachEvent("on" + event, func);
+/*
+ * Execute a function func on the element at the event 
+ */
+function addEvent(element, event, func) {
+    if (element.attachEvent) //IE
+        element.attachEvent("on" + event, func);
     else
-        obj.addEventListener(event, func, true);
+        element.addEventListener(event, func, true);
 }
 
-function focus(input) { // Focus at the end of the input
+/* 
+ * Focus hack : focus at the end of the input
+ */
+function focus(input) {
 	input.focus();
 	input.value = input.value;
 }
 
+/*
+ * Add a new entry.
+ */
 function addEntry() {
 	var entries = document.getElementById('entries');
 	var entry_template = document.getElementById('entry_template');
@@ -21,9 +30,9 @@ function addEntry() {
 	new_entry.setAttribute('id', '');
 	addEvent(new_entry, 'mouseover', function() {
 		if (new_entry.getElementsByClassName('content_edit')[0].style.display == 'none')
-			new_entry.getElementsByClassName('actions')[0].style.display = 'block'; 
+			new_entry.getElementsByClassName('actions')[0].style.visibility = 'visible'; 
 	});
-	addEvent(new_entry, 'mouseout', function() { new_entry.getElementsByClassName('actions')[0].style.display = 'none'; });
+	addEvent(new_entry, 'mouseout', function() { new_entry.getElementsByClassName('actions')[0].style.visibility = 'hidden'; });
 	addEvent(new_entry.getElementsByClassName('content')[0], 'click', function() { editEntry(new_entry); });
 	addEvent(new_entry.getElementsByClassName('action edit')[0], 'click', function() { editEntry(new_entry); });
 	addEvent(new_entry.getElementsByClassName('action done')[0], 'click', function() { doneEntry(new_entry); });
@@ -32,15 +41,21 @@ function addEntry() {
 	editEntry(new_entry);
 }
 
+/*
+ * Start editing an entry.
+ */
 function editEntry(entry) {
 	var content = entry.getElementsByClassName('content')[0];
 	var content_edit = entry.getElementsByClassName('content_edit')[0];
 	
-	content.style.display = 'none';
+	content.style.visibility = 'hidden';
 	content_edit.style.display = 'block';
 	focus(content_edit);
 }
 
+/*
+ * Finished editing an entry.
+ */
 function editEntryDone(entry) {
 	var content = entry.getElementsByClassName('content')[0];
 	var content_edit = entry.getElementsByClassName('content_edit')[0];
@@ -51,31 +66,40 @@ function editEntryDone(entry) {
 	content_edit.blur();
 	content.innerHTML = content_edit.value;
 	content_edit.style.display = 'none';
-	content.style.display = 'block';
+	content.style.visibility = 'visible';
 }
 
+/*
+ * Mark an entry as done.
+ */
 function doneEntry(entry) {
 	var content = entry.getElementsByClassName('content')[0].style.textDecoration = 'line-through';
 	entry.getElementsByClassName('action done')[0].style.visibility = 'hidden';
 }
 
+/*
+ * Mark an entry as undone.
+ */
 function undoEntry(entry) {
 	var content = entry.getElementsByClassName('content')[0].style.textDecoration = '';
 	entry.getElementsByClassName('action done')[0].style.visibility = 'visible';
 }
 
+/*
+ *	Remove all the entry with content_edit's value empty.
+ */
 function cleanEntries() {
 	var entries = document.getElementById('entries').getElementsByClassName('entry');
 	
 	for (var i = 0; i < entries.length; i++) {
 		var content_edit = entries[i].getElementsByClassName('content_edit')[0];
-		if (content_edit.value == '')
+		if (content_edit.value == '' && entries[i].id != 'entry_template')
 			entries[i].parentNode.removeChild(entries[i]);
 		else
 			content_edit.blur();
 	}
 	
-	if (entries.length == 1) // Automatically add a first entrys
+	if (entries.length == 1) // Automatically add a first entry
 		addEntry();
 }
 
